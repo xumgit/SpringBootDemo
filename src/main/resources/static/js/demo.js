3ce1fea291a6defa66579aea248cd82ae118ac4a
meng.xu <meng.xu@tpv-tech.com>
2018-07-20 09:31:05 +0800

    fix BootStrap Problem

--- src/main/resources/static/js/demo.js
+++ src/main/resources/static/js/demo.js
@@ -30,4 +30,12 @@ $(function(){
 	}).on("deselected.rs.jquery.bootgrid", function(e, rows){
 		console.log("deselected");
 	});
+
+	fixBootStrapProblem();
+	function fixBootStrapProblem() {
+		$("span.fa-refresh").parent().css({"height": "34px", "width": "50px"});
+		$("span.dropdown-text").parent().css({"height": "34px", "width": "50px"});
+		$("button[title='Refresh']").html("<span class=\"icon fa fa-refresh\"></span><span class=\"glyphicon glyphicon-refresh\"></span>");
+		$("<span class=\"glyphicon glyphicon-list\"></span>").appendTo(".fa-th-list");
+	}
 });
