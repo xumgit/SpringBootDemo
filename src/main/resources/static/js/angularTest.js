b986d8c01a2337dd88f489e486b09517b6032b48
meng.xu <meng.xu@tpv-tech.com>
2018-10-16 17:39:22 +0800

    study angular add agular factory

--- src/main/resources/static/js/angularTest.js
+++ src/main/resources/static/js/angularTest.js
@@ -2,10 +2,27 @@
  * 
  */
 
-var myAugulars = angular.module('myAugular', ['ngAnimate','ngRoute']);
+var myAugulars = angular.module('myAugular', ['ngAnimate','ngRoute','ngCookies']);
 
-myAugulars.controller('loginController', ['$scope', '$rootScope','$timeout', 'Configure','$cookieStore', 'locals', 
-    function($scope, $rootScope, $timeout, Configure, $cookieStore, locals) {
+myAugulars.factory('locals', ['$window', function($window){
+                   return{
+                      set: function(key, value){
+                        $window.localStorage[key] = value;
+                      },
+                      get: function(key, defaultValue){
+                        return  $window.localStorage[key] || defaultValue;
+                      },
+                      setObject: function(key, value){
+                        $window.localStorage[key] = JSON.stringify(value);
+                      },
+                      getObject: function(key) {
+                        return JSON.parse($window.localStorage[key] || '{}');
+                      }
+                    };
+                 }]);
+
+myAugulars.controller('loginController', ['$scope', '$rootScope','$timeout', 'Configure','$cookiesProvider', 'locals', 
+    function($scope, $rootScope, $timeout, Configure, $cookiesProvider, locals) {
     $scope.user = {userName:'', userPassword:'', remeberMe:false, kaptcha:'', captchaguid:''}; 
     $scope.login = function() {
         console.log("$scope.user.userName=" + $scope.user.userName);
