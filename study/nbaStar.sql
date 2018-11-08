2817b4c27905dd874f5084edca940d6c75759fb5
meng.xu <meng.xu@tpv-tech.com>
2018-11-08 14:48:05 +0800

    modify sql script

--- study/nbaStar.sql
+++ study/nbaStar.sql
@@ -14,7 +14,7 @@
  Date: 11/05/2018 20:42:04 PM
 */
 
-SET NAMES utf8mb4;
+SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
@@ -36,7 +36,15 @@ CREATE TABLE `nbaStar` (
 --  Records of `nbaStar`
 -- ----------------------------
 BEGIN;
-INSERT INTO `nbaStar` VALUES ('1', '23', 'James', 'PF', '骑士', 'james.png', '1988'), ('2', '30', 'Curry', 'SG', '勇士', 'curry.png', '1865'), ('3', '7', 'Anthony', 'C', '尼克斯', 'anthony.png', '1495'), ('4', '3', 'Paul', 'PG', '快船', 'paul.png', '1600'), ('5', '13', 'Harden', 'SF', '火箭', 'harden.png', '1813'), ('6', '2', 'Irving', 'PG', '骑士', 'irving.png', '1361'), ('7', '7', 'Lin', 'PG', '篮网', 'lin.png', '1200'), ('8', '3', 'Wade', 'SG', '热火', 'wade.png', '1532'), ('9', '0', 'Westbrook', 'SG', '雷霆', 'westbrook.png', '2017'), ('10', '35', 'Durant', 'SF', '勇士', 'durant.png', '1721');
+INSERT INTO `nbaStar` VALUES 
+('1', '23', 'James', 'PF', 'CLE', 'james.png', '1988'), 
+('2', '30', 'Curry', 'SG', 'GSW', 'curry.png', '1865'), 
+('3', '7', 'Anthony', 'C', 'NYN', 'anthony.png', '1495'), 
+('4', '3', 'Paul', 'PG', 'LAC', 'paul.png', '1600'), 
+('5', '13', 'Harden', 'SF', 'HOU', 'harden.png', '1813'), 
+('6', '2', 'Irving', 'PG', 'CLE', 'irving.png', '1361'), 
+('7', '7', 'Lin', 'PG', 'NET', 'lin.png', '1200'), 
+('8', '3', 'Wade', 'SG', 'MIA', 'wade.png', '1532'), 
+('9', '0', 'Westbrook', 'SG', 'OCT', 'westbrook.png', '2017'), 
+('10', '35', 'Durant', 'SF', 'ASW', 'durant.png', '1721');
 COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
